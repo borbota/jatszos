dc3616ef0a04a36100aa938925fb7a50475b136c
borbota <tothborbala@yahoo.es>
2017-08-29 15:13:59 +0200

    Update network_test.py

--- network_test.py
+++ network_test.py
@@ -2,25 +2,28 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import itertools
+import csv
 
-f = open("notices_cpv_decoded_test.csv", "rb")
+f = open("notices_cpv_decoded_just_main_cpvcategory.csv", "rb")
 
 notice_list = []
-aktualis_notice = "15135-2016"
-kategoria_list = []
-with open("cpv_cpv_network_by_notice_test.csv", "w") as output:
+aktualis_notice = "27086-2016"
+
+with open("cpv_cpv_network_by_notice.csv", "wb") as output:
+    kategoria_list = []
+    csv_out = csv.writer(output)
     for line in f.readlines()[1:]:
         notice = line.split('\t')[0].lower()
+        kategoria = line.split('\t')[1].rstrip("\n")
         if notice != aktualis_notice:
             my_list = [(kategoria_list[i], kategoria_list[j]) for i in\
                    range(len(kategoria_list)) for j in range(i + 1, len(kategoria_list))]
-            print str(my_list)
-            kategoria = line.split('\t')[1].lower().rstrip("\n")
+            for row in my_list:
+                csv_out.writerow(row)
+            kategoria_list = []
             kategoria_list.append(kategoria)
             aktualis_notice = notice
-
         elif notice == aktualis_notice:
-            kategoria = line.split('\t')[1].lower().rstrip("\n")
             if kategoria not in kategoria_list:
                 kategoria_list.append(kategoria)
             else:
