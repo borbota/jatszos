c8b9cf677781196c32aebb3b652f8f7a8e41420f
borbota <tothborbala@yahoo.es>
2017-08-29 16:22:13 +0200

    Update network_test.py

--- network_test.py
+++ network_test.py
@@ -10,19 +10,21 @@ notice_list = []
 aktualis_notice = "27086-2016"
 
 with open("cpv_cpv_network_by_notice.csv", "wb") as output:
-    kategoria_list = []
     csv_out = csv.writer(output)
+    kategoria_list = []
     for line in f.readlines()[1:]:
         notice = line.split('\t')[0].lower()
         kategoria = line.split('\t')[1].rstrip("\n")
         if notice != aktualis_notice:
-            my_list = [(kategoria_list[i], kategoria_list[j]) for i in\
-                   range(len(kategoria_list)) for j in range(i + 1, len(kategoria_list))]
-            for row in my_list:
-                csv_out.writerow(row)
-            kategoria_list = []
-            kategoria_list.append(kategoria)
-            aktualis_notice = notice
+            if len(kategoria_list) > 1:
+                my_list = [(kategoria_list[i], kategoria_list[j]) for i in\
+                       range(len(kategoria_list)) for j in range(i + 1, len(kategoria_list))]
+                for row in my_list:
+                    csv_out.writerow(row)
+                kategoria_list = []
+                kategoria_list.append(kategoria)
+            else:
+                pass
         elif notice == aktualis_notice:
             if kategoria not in kategoria_list:
                 kategoria_list.append(kategoria)
@@ -30,5 +32,6 @@ with open("cpv_cpv_network_by_notice.csv", "wb") as output:
                 pass
         else:
             print "nem ok"
+        aktualis_notice = notice
 
 f.close()
