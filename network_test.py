726025817116bbbd647095be00d980790b381e67
borbota <tothborbala@yahoo.es>
2017-08-29 12:23:33 +0200

    Update network_test.py

--- network_test.py
+++ network_test.py
@@ -5,15 +5,12 @@ import itertools
 
 f = open("notices_cpv_decoded_test.csv", "rb")
 
-
 notice_list = []
 aktualis_notice = "15135-2016"
 kategoria_list = []
 with open("cpv_cpv_network_by_notice_test.csv", "w") as output:
     for line in f.readlines()[1:]:
-
         notice = line.split('\t')[0].lower()
-        #if notice not in notice_list:
         if notice != aktualis_notice:
             my_list = [(kategoria_list[i], kategoria_list[j]) for i in\
                    range(len(kategoria_list)) for j in range(i + 1, len(kategoria_list))]
